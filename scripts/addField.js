9c9bfcd6a37a899ecd5314c89f06d85554e31df8
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-08-05 18:48:39 -0300

    Inicio do JS e banner

---
+++ scripts/addField.js
@@ -0,0 +1,12 @@
+// Procurar o botão
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+//Ao clicar no botão
+
+//Executar ação
+function cloneField() {
+  console.log('Clicou no botão.')
+}
+//Duplicar os campos
+
+//Colocar na página
